89cee50f01fb8b8c14eb193fb85c8cd231547115
Tonic78 <guymertz@gmaiL.com>
2020-05-23 12:29:30 +0200

    add pull to refresh on messages screen

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Platform, StatusBar } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
+import Constants from "expo-constants";
 
 function Screen({ children }) {
   return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
@@ -7,7 +8,8 @@ function Screen({ children }) {
 
 const styles = StyleSheet.create({
   screen: {
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    paddingTop: Constants.statusBarHeight,
+    flex: 1,
   },
 });
 
