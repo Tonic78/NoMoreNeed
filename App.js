f05ddab5cdb9ec9697bef0d8416bc0ecc2abb4b2
Tonic78 <guymertz@gmaiL.com>
2020-05-23 16:02:20 +0200

    add text input component

--- App.js
+++ App.js
@@ -12,7 +12,12 @@ import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-  return <ListingsScreen />;
+  return (
+    <Screen>
+      <AppTextInput placeholder="Username" icon="email" />
+    </Screen>
+  );
 }
