805c4eb48963fc3403b9f4fa4cdf82174e5d530f
Tonic78 <guymertz@gmaiL.com>
2020-05-23 14:38:40 +0200

    add accountscreen with items

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -33,6 +33,7 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     padding: 15,
+    backgroundColor: "#fff",
   },
   detailsContainer: {
     marginLeft: 10,
---
+++ app/screens/AccountScreen.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+import Screen from "../components/Screen";
+import ListItem from "../components/ListItem";
+import Icon from "../components/Icon";
+import ListItemSeparator from "../components/ListItemSeparator";
+import colors from "../config/colors";
+
+function AccountScreen() {
+  return (
+    <Screen style={styles.screen}>
+      <View style={styles.container}>
+        <ListItem
+          title="Dark Knight"
+          subTitle="batman@email.com"
+          image={require("../assets/batman.png")}
+        />
+      </View>
+      <View style={styles.container}>
+        <ListItem
+          title="My Listings"
+          IconComponent={
+            <Icon
+              name="format-list-bulleted"
+              backgroundColor={colors.primary}
+            />
+          }
+        />
+        <ListItemSeparator />
+        <ListItem
+          title="My Messages"
+          IconComponent={
+            <Icon name="email" backgroundColor={colors.secondary} />
+          }
+        />
+      </View>
+      <ListItem
+        title="Log Out"
+        IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+  },
+  screen: {
+    backgroundColor: colors.light,
+  },
+});
+
+export default AccountScreen;
